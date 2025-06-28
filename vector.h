0fdda511b9bd9017187fedb42cd7952fe1f1bbfc
Yiming <2018565194@qq.com>
2025-06-28 21:21:17 +0800

    Create vector.h

---
+++ vector.h
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<iostream>
+namespace yiming
+{
+	template<class T>
+	class vector
+	{
+	public:
+		typedef T* iterator;
+		vector()
+			:_start(nullptr)
+			, _finish(nullptr)
+			, _endofstorage(nullptr)
+		{
+
+		}
+		void reverse(size_t n)
+		{
+			if (n > _capacity())
+			{
+				T* tmp = new T[n];
+			//当start不为空时，拷贝旧数据到新空间
+				if (_start)
+				{
+					memcpy(tmp, _start, sizeof(T) * _size());
+					delete[]_start;
+				}
+				_start = tmp;
+				_finish = _start + size();
+				_endofstorage = _start + n;
+			}
+		}
+
+		size_ t size()const
+		{
+			return _finsh - _start;
+		}
+		size_t capacity()const
+		{
+			return  _endofstorage - _start;
+		}
+		void push_back(const T& x)
+		{
+			if (_finish == _endofstorage)
+			{
+				reserve(capacity() == 0 ? 4;capacity() * 2);
+			}
+			*_finsh = x;
+			++_finish;
+		}
+		
+		
+	private:
+		iterator _start;
+		iterator _finsh;
+		iterator _endofstorage;
+
+	};
+}
