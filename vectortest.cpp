0a799b359973002cfd0fe95a96eeb4d4471e3ca6
Yiming <2018565194@qq.com>
2025-06-29 16:25:11 +0800

    Update vectortest.cpp

--- vectortest.cpp
+++ vectortest.cpp
@@ -1,11 +1,61 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include"vector.h"
-
-namespace yiming
+﻿#define _CRT_SECURE_NO_WARNINGS
+//#include"vector.h"
+#include<iostream>
+#include<string>
+#include<stdio.h>
+using namespace std;
+//
+//namespace yiming
+//{
+//	void test01()
+//	{
+//		vector<int> v;
+//		
+//	}
+//}
+int main()
 {
-	void test01()
+
+	string a = "hello world";
+
+	string b = a;
+
+	if (a.c_str() == b.c_str())
+
 	{
-		vector<int> v;
-		v.push_back(1);
+
+		cout << "true" << endl;
+
 	}
+
+	else cout << "false" << endl;
+
+	string c = b;
+
+	c = "";
+
+	if (a.c_str() == b.c_str())
+
+	{
+
+		cout << "true" << endl;
+
+	}
+
+	else cout << "false" << endl;
+
+	a = "";
+
+	if (a.c_str() == b.c_str())
+
+	{
+
+		cout << "true" << endl;
+
+	}
+
+	else cout << "false" << endl;
+
+	return 0;
+
 }
