b180a74f98b90b69b4c8053e0516c588dec07743
Yiming <2018565194@qq.com>
2025-06-30 21:05:34 +0800

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,9 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include"vector.h"
-void Print(yiming::vector<int>& v)
+
+void yiming::Print(yiming::vector<int>& v)
 {
 	for (auto e : v)
 	{
 		cout << e << " ";
 	}
 }
+
