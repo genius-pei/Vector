d678cff9cd2aa119044ccf74599781759cd20f3a
Yiming <2018565194@qq.com>
2025-06-30 21:05:49 +0800

    Update vectortest.cpp

--- vectortest.cpp
+++ vectortest.cpp
@@ -1,10 +1,16 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include"vector.h"
 
-
-	
-
 	void test01()
 	{
-		vector<int> a;
+		yiming::vector<int> a;
+		a.push_back(1);
+		a.push_back(2);
+		a.push_back(3);
+		Print(a);
+	}
+	int main()
+	{
+		test01();
+		return 0;
 	}
