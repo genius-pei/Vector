ff4738b2bbffb805bc1993cc3181b78f9d292b41
Yiming <2018565194@qq.com>
2025-06-30 21:34:58 +0800

    Update vectortest.cpp

--- vectortest.cpp
+++ vectortest.cpp
@@ -4,9 +4,12 @@
 	void test01()
 	{
 		yiming::vector<int> a;
+		a.insert(a.begin(), 0);
 		a.push_back(1);
 		a.push_back(2);
 		a.push_back(3);
+		Print(a);
+		a.pop_back();
 		Print(a);
 	}
 	int main()
